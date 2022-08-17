f0e6dc2b02c6c33bf953e325f24d0b216014863a
Rohit Prasad <pdrohit674@gmail.com>
2022-08-18 00:30:31 +0530

    feat: responsive navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Navbar() {
+  const [showNavbar, setShowNavbar] = useState(true)
+
   return (
-    <div className="flex h-16 text-2xl justify-between items-center bg-white-bg fixed top-0 left-0 right-0">
-      <div className="font-bold ml-8 cursor-pointer">TaxEasy</div>
-      <div className="flex mr-4 text-xl gap-4">
-        <div className="cursor-pointer py-1">Search</div>
-        <div className="cursor-pointer py-1">Pricing</div>
-        <div className="cursor-pointer py-1">About us</div>
-        <div className="cursor-pointer py-1">Contact us</div>
+    <div className="navbar-background flex h-20 justify-between items-center bg-white-bg fixed top-0 left-0 right-0 md:text-2xl">
+      <div className="font-bold ml-8 cursor-pointer text-4xl md:text-2xl">
+        TaxEasy
+      </div>
+      <div className="mr-4 hidden md:text-xl md:gap-4 md:flex">
+        <div className="cursor-pointer py-1 hover:underline">Search</div>
+        <div className="cursor-pointer py-1 hover:underline">Pricing</div>
+        <div className="cursor-pointer py-1 hover:underline">About us</div>
+        <div className="cursor-pointer py-1 hover:underline">Contact us</div>
         <button className="cursor-pointer white-btn px-4 py-1 rounded">
           Login
         </button>
@@ -16,6 +20,35 @@ function Navbar() {
           Get Started
         </button>
       </div>
+      <div
+        onClick={() => setShowNavbar(!showNavbar)}
+        className="text-4xl md:hidden cursor-pointer">
+        {showNavbar ? 'Close' : 'Open'}
+      </div>
+      {showNavbar && (
+        <div className="flex flex-col gap-6 justify-center absolute top-48 w-full h-full border-4 mt-8 md:hidden">
+          <div className="cursor-pointer px-4 hover:underline text-2xl">
+            Search
+          </div>
+          <div className="cursor-pointer px-4 hover:underline text-2xl">
+            Pricing
+          </div>
+          <div className="cursor-pointer px-4 hover:underline text-2xl">
+            About us
+          </div>
+          <div className="cursor-pointer px-4 hover:underline text-2xl mb-8">
+            Contact us
+          </div>
+          <div className="flex gap-2 justify-evenly px-8">
+            <button className="cursor-pointer white-btn px-4 py-4 rounded flex-grow">
+              Login
+            </button>
+            <button className="cursor-pointer primary-btn px-4 py-4 rounded flex-grow">
+              Get Started
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
