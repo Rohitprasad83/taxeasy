4b756e1e6757a6fda2ced308e6388c6b3a7191e0
Rohit Prasad <pdrohit674@gmail.com>
2022-08-17 22:36:21 +0530

    feat: added Roboto Google font

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className="flex h-16 text-2xl justify-between items-center bg-white-bg">
-      <div className="font-bold ml-2 cursor-pointer">TaxEasy</div>
-      <div className="flex mr-2 text-xl gap-2">
-        <div className="cursor-pointer">Search</div>
-        <div className="cursor-pointer">About us</div>
-        <div className="cursor-pointer">Contact us</div>
+    <div className="flex h-16 text-2xl justify-between items-center bg-white-bg fixed top-0 left-0 right-0">
+      <div className="font-bold ml-8 cursor-pointer">TaxEasy</div>
+      <div className="flex mr-4 text-xl gap-4">
+        <div className="cursor-pointer py-1">Search</div>
+        <div className="cursor-pointer py-1">Pricing</div>
+        <div className="cursor-pointer py-1">About us</div>
+        <div className="cursor-pointer py-1">Contact us</div>
         <button className="cursor-pointer white-btn px-4 py-1 rounded">
           Login
         </button>
