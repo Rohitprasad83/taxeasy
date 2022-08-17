3139c3e5af33fd0ca341f1c7f2dc8de51c067d06
Rohit Prasad <pdrohit674@gmail.com>
2022-08-17 20:44:03 +0530

    feat: initial commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar, Footer } from 'components/index'
+function Homepage() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export { Homepage }
