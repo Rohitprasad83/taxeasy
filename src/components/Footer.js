3c63bfb1cdf348cdaff5fd6d3ec4483d3d707059
Rohit Prasad <pdrohit674@gmail.com>
2022-08-18 21:39:33 +0530

    feat: basic layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,30 @@
 import React from 'react'
 
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <div className="flex fixed bottom-0">
+      <div> Terms - ITR </div> <div> Terms - Financial Products & GST </div>
+      <div> Privacy - ITR </div> <div> Privacy - Financial Products & GST </div>
+      <div> Legal </div>
+      <div>
+        <div>
+          <i className="fa-brands fa-facebook"> </i>
+        </div>
+        <div>
+          <i className="fa-brands fa-twitter"> </i>
+        </div>
+        <div>
+          <i className="fa-brands fa-linkedin"> </i>
+        </div>
+        <div>
+          <i className="fa-brands fa-square-instagram"></i>
+        </div>
+        <div>
+          <i className="fa-brands fa-youtube"></i>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export { Footer }
