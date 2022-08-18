18f8f8a0a75937998eb77b487b47ca6840f194ff
Rohit Prasad <pdrohit674@gmail.com>
2022-08-18 22:12:30 +0530

    feat: styling for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,25 +2,36 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className="flex fixed bottom-0">
-      <div> Terms - ITR </div> <div> Terms - Financial Products & GST </div>
-      <div> Privacy - ITR </div> <div> Privacy - Financial Products & GST </div>
-      <div> Legal </div>
-      <div>
+    <div className="flex flex-col justify-center items-center content-center gap-4 fixed bottom-0 bg-light-bg text-dark-bg text-xl w-full h-max p-4">
+      <div className="flex gap-4 text-sm flex-wrap">
+        <div className="w-max hover:underline cursor-pointer">Terms - ITR</div>
+        <div className="w-max hover:underline cursor-pointer">
+          Terms - Financial Products & GST
+        </div>
+        <div className="w-max hover:underline cursor-pointer">
+          Privacy - ITR
+        </div>
+        <div className="w-max hover:underline cursor-pointer">
+          Privacy - Financial Products & GST
+        </div>
+        <div className="w-max hover:underline cursor-pointer"> Legal </div>
+      </div>
+
+      <div className="flex gap-4 text-2xl md:gap-8">
         <div>
-          <i className="fa-brands fa-facebook"> </i>
+          <i className="fa-brands fa-facebook cursor-pointer"> </i>
         </div>
         <div>
-          <i className="fa-brands fa-twitter"> </i>
+          <i className="fa-brands fa-twitter cursor-pointer"> </i>
         </div>
         <div>
-          <i className="fa-brands fa-linkedin"> </i>
+          <i className="fa-brands fa-linkedin cursor-pointer"> </i>
         </div>
         <div>
-          <i className="fa-brands fa-square-instagram"></i>
+          <i className="fa-brands fa-square-instagram cursor-pointer"></i>
         </div>
         <div>
-          <i className="fa-brands fa-youtube"></i>
+          <i className="fa-brands fa-youtube cursor-pointer"></i>
         </div>
       </div>
     </div>
