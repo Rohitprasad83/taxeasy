904b46962c0e2e3aaa091b3e7ced2cbdf8d6043d
Ratan kumar Singh <ratanrathour5@gmail.com>
2022-08-23 21:35:41 +0530

    Initial Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 function Footer() {
-  return <div>Footer</div>
+  return <div><form action="" class="form-Tryforfree" method="POST">
+  <h2>Ready to get started?</h2>
+  <button>Try for free</button>
+  <button>Schedule a demo -></button>
+</form></div>
 }
 
 export { Footer }
