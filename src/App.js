34b8e7a64728739c3923a3326951643f805528ac
subham2111 <subhamaggarwal2111@gmail.com>
2022-09-05 01:49:08 +0530

    routing done linking left

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css'
-import { Homepage, Login, Register, Profile } from 'pages/'
+import { Homepage, Login,Register, Profile } from 'pages/'
+// import { Register } from 'pages/Register'
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      {/* <Homepage /> */} <Login /> {/* <Register /> */} {/* <Profile /> */}{' '}
+    <div className='APP'>
+       <Login />
+       <nav>
+        <Link to="/register">sign up</Link>
+      </nav>
     </div>
   )
 }
+
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+// import { BrowserRouter } from "react-router-dom"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom"
 import reportWebVitals from './reportWebVitals'
+import { Homepage, Login,Register, Profile } from 'pages/'
 import { store } from 'app/store'
 import { Provider } from 'react-redux'
 
@@ -10,7 +17,17 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <BrowserRouter>
+      {/* <App /> */}
+      <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/home" element={<Homepage />} />
+      {/* <Route path="invoices" element={<Invoices />} /> */}
+    </Routes>
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>
 )
